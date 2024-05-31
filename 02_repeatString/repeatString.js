c708484a2dcd694b58142093f5cba4149860979d
Pro696969 <krishnakumaarmc@gmail.com>
2024-05-31 19:36:14 +0530

    02 sol

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(s,num) {
+    let s2="";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for(let i=0; i<num; i++) {
+        s2+=s
+    }
+    return s2;
 
 };
 
