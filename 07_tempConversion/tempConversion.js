7c0789c40ad8a540bc94a31972a6613f8b285a66
Pro696969 <krishnakumaarmc@gmail.com>
2024-05-31 22:12:19 +0530

    07 sol

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let C = (F-32)/1.8
+  return Math.round(C*10)/10
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let F = (C*1.8) + 32
+  return Math.ceil(F*10)/10
+
 };
 
 // Do not edit below this line
